f1a0f5bc0a0700234b670f3e068acd8874010409
AllisonLDolan <145593596+AllisonLDolan@users.noreply.github.com>
2025-03-21 13:39:24 -0500

    new endpoint retry

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use('/mydata', mydataRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
